9fadd776dcd9b79e045d99b9987b93d3677c3cf4
BrunoCBarbosa <bruno.cbarbosa90@gmail.com>
2018-06-19 00:40:06 -0300

    teste 32

--- Industria/tests/SampleTest.php
+++ Industria/tests/SampleTest.php
@@ -23,17 +23,14 @@ class SampleTest extends PHPUnit_Framework_TestCase
 
 //     }
     
-public function testPushAndPop()
-{
-  $stack = array();
-  $this->assertEquals(0, count($stack));
-  array_push($stack, 'foo');
-  $this->assertEquals('foo', $stack[count($stack)-1]);
-  $this->assertEquals(1, count($stack));
-  $this->assertEquals('foo', array_pop($stack));
-  $this->assertEquals(0, count($stack));
-}
-
+    public function testeRelatorio(){
+        
+        $data_inicio = new DateTime('2018-08-03');
+        $data_fim = new DateTime('2018-08-06');
+        
+        $this->assertTrue($data_inicio < $data_fim);
+        
+    }
     
     
 }
